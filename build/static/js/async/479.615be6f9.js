dfc66ad5e0be2b485b5b3a5db976a373df716223
DeFFeCT139 <hodak_1994@bk.ru>
2024-05-09 14:55:24 +0300

    delite build

--- build/static/js/async/479.615be6f9.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see 479.615be6f9.js.LICENSE.txt */
-(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([["479"],{389:function(e,o,s){"use strict";s.r(o);var a=s("5893");o.default=()=>(0,a.jsx)("div",{className:"Home"})}}]);
