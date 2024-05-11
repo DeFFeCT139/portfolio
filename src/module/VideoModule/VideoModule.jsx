8eab88f43f68966adb11f8b2d7668331c2e76ea8
DeFFeCT139 <hodak_1994@bk.ru>
2024-05-11 19:44:50 +0300

    table fix

--- src/module/VideoModule/VideoModule.jsx
+++ src/module/VideoModule/VideoModule.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PlayIcon from './components/PlayIcon/playIcon';
 import style from './css/VideoModule.module.css'
 import ReactPlayer from 'react-player/youtube'
@@ -5,27 +6,45 @@ import ReactPlayer from 'react-player/youtube'
 
 const VideoModule = () => {
 
-  let list = [
-    {path:'https://www.youtube.com/watch?v=FoSgDtqb0Zc'},
-    {path:'https://www.youtube.com/watch?v=FoSgDtqb0Zc'},
-    {path:'https://www.youtube.com/watch?v=FoSgDtqb0Zc'},
-    {path:'https://www.youtube.com/watch?v=FoSgDtqb0Zc'},
-    {path:'https://www.youtube.com/watch?v=FoSgDtqb0Zc'},
-    {path:'https://www.youtube.com/watch?v=FoSgDtqb0Zc'},
-    {path:'https://www.youtube.com/watch?v=FoSgDtqb0Zc'},
-    {path:'https://www.youtube.com/watch?v=FoSgDtqb0Zc'},
-    {path:'https://www.youtube.com/watch?v=FoSgDtqb0Zc'},
-    {path:'https://www.youtube.com/watch?v=FoSgDtqb0Zc'},
-    {path:'https://www.youtube.com/watch?v=FoSgDtqb0Zc'},
-    {path:'https://www.youtube.com/watch?v=FoSgDtqb0Zc'},
-  ]
+  const [links] = useState([
+    {path:'https://www.youtube.com/watch?v=GNrdg3PzpJQ&t=8782s'},
+    {path:'https://www.youtube.com/watch?v=noXnxdNSp4A'},
+    {path:'https://www.youtube.com/watch?v=x1RJ5Q09PqM'},
+    {path:'https://www.youtube.com/watch?v=mxquElJC_qM'},
+    {path:'https://www.youtube.com/watch?v=LpEwssOYRKA'},
+    {path:'https://www.youtube.com/watch?v=WQJ4pYtlN5E'},
+    {path:'https://www.youtube.com/watch?v=9zewiGf7j-A'},
+    {path:'https://www.youtube.com/watch?v=ifTF3ags0XI'},
+    {path:'https://www.youtube.com/watch?v=BWNDgjAv78M'},
+    {path:'https://www.youtube.com/watch?v=zDlg64fsQow'},
+    {path:'https://www.youtube.com/watch?v=adcijRTI_8E'},
+    {path:'https://www.youtube.com/watch?v=t2f6kg8mxwM'},
+    {path:'https://www.youtube.com/watch?v=eOWxKkHakyY'},
+    {path:'https://www.youtube.com/watch?v=CGLdH5ORX-Y'},
+    {path:'https://www.youtube.com/watch?v=APycW2hRdBg'},
+    {path:'https://www.youtube.com/watch?v=c3JGBdxfYcU&t=1057s'},
+    {path:'https://www.youtube.com/watch?v=K9vBpIq8Wck'},
+    {path:'https://www.youtube.com/watch?v=Tn6-PIqc4UM'},
+    {path:'https://www.youtube.com/watch?v=8OHe6chCWTE'},
+    {path:'https://www.youtube.com/watch?v=R_agd5qZ26Y'},
+    {path:'https://www.youtube.com/watch?v=8LlitCpjoxA'},
+    {path:'https://www.youtube.com/watch?v=iIG7eEZoil8'},
+    {path:'https://www.youtube.com/watch?v=lr1rYnUubpQ'},
+    {path:'https://www.youtube.com/watch?v=1u-ol271d7Q'},
+    {path:'https://www.youtube.com/watch?v=6gf-rSjkUik'},
+    {path:'https://www.youtube.com/watch?v=6O4z5ksuCi8'},
+    {path:'https://www.youtube.com/watch?v=Iz1NvqG7wTc'},
+    {path:'https://www.youtube.com/watch?v=_gQaygjm_hg'},
+    {path:'https://www.youtube.com/watch?v=MRNGw08i6S4'},
+    {path:'https://www.youtube.com/watch?v=sNKClZoiHNk'},
+  ])
 
     return (
       <div className={style.VideoModule}>
         <div className="container">
           <div className={style.VideoModule_list}>
-            {list.map(item => 
-              <div key={list.indexOf(item)} className={style.VideoModule_item}>
+            {links.map(item => 
+              <div key={links.indexOf(item)} className={style.VideoModule_item}>
                 <ReactPlayer
                   light
                   url={item.path}
